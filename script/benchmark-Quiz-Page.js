c6bdd9d55afce0077505d41606fb8423bf641067
Emanuele Umberto Mangano <emanuelemangano15@gmail.com>
2024-03-21 09:10:47 +0100

    result-fix

--- script/benchmark-Quiz-Page.js
+++ script/benchmark-Quiz-Page.js
@@ -95,7 +95,7 @@ const arreyOfQuestions = [
     correct_answer: "Objective-C",
     incorrect_answers: ["C#", "Ruby", "C++"],
   },
-];
+]
 
 const mediumArray = [
   {
@@ -285,7 +285,7 @@ const mediumArray = [
     correct_answer: "Intel 4004",
     incorrect_answers: ["Intel 486SX", "TMS 1000", "AMD AM386"],
   },
-];
+]
 
 const hardArray = [
   {
@@ -571,59 +571,59 @@ const hardArray = [
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
-];
+]
 
 //prendere html
-const timerH2 = document.querySelector(" h2");
-const h3 = document.querySelector(" .h3 h3");
-const p = document.querySelector("footer p");
-const borderTimer = document.querySelector(".border-timer");
-const pCorrect = document.querySelector(".correct");
-const pWrong = document.querySelector(".wrong");
-const section1 = document.querySelector(".sect1");
-const section2 = document.querySelector(".sect2");
+const timerH2 = document.querySelector(" h2")
+const h3 = document.querySelector(" .h3 h3")
+const p = document.querySelector("footer p")
+const borderTimer = document.querySelector(".border-timer")
+const pCorrect = document.querySelector(".correct")
+const pWrong = document.querySelector(".wrong")
+const section1 = document.querySelector(".sect1")
+const section2 = document.querySelector(".sect2")
 //punteggio risposte corette
-const numerOfquestionP = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numerOfquestionP = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //randomizzare bottoni
-const questionArr = []; //tutte le risposte
-const arrOfTitle = []; //tutte i titoli
+const questionArr = [] //tutte le risposte
+const arrOfTitle = [] //tutte i titoli
 //creare bottoni
 //Fisher–Yates shuffle!!!
 function shuffle(questionArr) {
-  let currentIndex = questionArr.length;
-  let randomIndex;
+  let currentIndex = questionArr.length
+  let randomIndex
 
   while (currentIndex > 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [questionArr[currentIndex], questionArr[randomIndex]] = [
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[questionArr[currentIndex], questionArr[randomIndex]] = [
       questionArr[randomIndex],
       questionArr[currentIndex],
-    ];
+    ]
   }
 
-  return questionArr;
+  return questionArr
 }
 
 for (let i = 0; i < arreyOfQuestions.length; i++) {
   const incorrectAnswers = questionArr.push(
     arreyOfQuestions[i].incorrect_answers
-  ); //easy
-  questionArr[i].push(arreyOfQuestions[i].correct_answer);
+  ) //easy
+  questionArr[i].push(arreyOfQuestions[i].correct_answer)
   //titoloriposte
-  arrOfTitle.push(arreyOfQuestions[i].question);
+  arrOfTitle.push(arreyOfQuestions[i].question)
   if (i > 8) {
     for (let j = 0; j < 20; j++) {
-      questionArr.push(mediumArray[j].incorrect_answers); //medium
-      questionArr[j + 10].push(mediumArray[j].correct_answer);
-      arrOfTitle.push(mediumArray[j].question);
-      shuffle(questionArr[j+10]);
+      questionArr.push(mediumArray[j].incorrect_answers) //medium
+      questionArr[j + 10].push(mediumArray[j].correct_answer)
+      arrOfTitle.push(mediumArray[j].question)
+      shuffle(questionArr[j + 10])
       if (j > 18) {
         for (let k = 0; k < 30; k++) {
-          questionArr.push(hardArray[k].incorrect_answers); //hard
-          questionArr[k + 30].push(hardArray[k].correct_answer);
-          arrOfTitle.push(hardArray[k].question);
-          shuffle(questionArr[k+30]);
+          questionArr.push(hardArray[k].incorrect_answers) //hard
+          questionArr[k + 30].push(hardArray[k].correct_answer)
+          arrOfTitle.push(hardArray[k].question)
+          shuffle(questionArr[k + 30])
         }
       }
     }
@@ -631,235 +631,197 @@ for (let i = 0; i < arreyOfQuestions.length; i++) {
 
   if (questionArr[i].length === 4) {
     if (i < 2) {
-      const btn = document.createElement("button");
-      section1.appendChild(btn);
+      const btn = document.createElement("button")
+      section1.appendChild(btn)
     } else if (i < 4) {
-      const btn = document.createElement("button");
-      section2.appendChild(btn);
+      const btn = document.createElement("button")
+      section2.appendChild(btn)
     }
   }
-  shuffle(questionArr[i]);
+  shuffle(questionArr[i])
 }
-console.log(questionArr);
-console.log(arrOfTitle);
+console.log(questionArr)
+console.log(arrOfTitle)
 //trovare bottoni
-const buttons = document.querySelectorAll(".button button");
-const button1 = buttons[0];
-button1.classList.add("btn1");
-const button2 = buttons[1];
-button2.classList.add("btn2");
-const button3 = buttons[2];
-button3.classList.add("btn3");
-const button4 = buttons[3];
-button4.classList.add("btn4");
+const buttons = document.querySelectorAll(".button button")
+const button1 = buttons[0]
+button1.classList.add("btn1")
+const button2 = buttons[1]
+button2.classList.add("btn2")
+const button3 = buttons[2]
+button3.classList.add("btn3")
+const button4 = buttons[3]
+button4.classList.add("btn4")
 //testo bottoni
 for (let i = 0; i < questionArr[0].length; i++) {
-  buttons[i].innerText = questionArr[0][i];
+  buttons[i].innerText = questionArr[0][i]
 }
-let totalResult = 0;
-const lengthArray = arreyOfQuestions.length;
+let totalResult = 0
+const lengthArray = arreyOfQuestions.length
 
 //contatore
 const timerCounter = function () {
-  let counterTimerColor = 0;
-  let count = 5;
-  let i = 0;
+  let counterTimerColor = 0
+  let count = 5
+  let i = 0
 
   const interval = setInterval(function () {
-    counterTimerColor += 1.69;
-    count--;
-    timerH2.innerHTML = count;
-    borderTimer.style.background = `conic-gradient(rgba(255, 255, 255, 0.13) 0% ${counterTimerColor}%,rgb(139, 228, 255) 0%)`;
+    counterTimerColor += 1.69
+    count--
+    timerH2.innerHTML = count
+    borderTimer.style.background = `conic-gradient(rgba(255, 255, 255, 0.13) 0% ${counterTimerColor}%,rgb(139, 228, 255) 0%)`
 
     if (counterTimerColor === 101.39999999999992) {
-      counterTimerColor = 1;
+      counterTimerColor = 1
     }
     button1.onclick = function () {
       for (let j = 0; j < buttons.length; j++) {
         if (buttons[j].innerText === arreyOfQuestions[i].correct_answer) {
-          buttons[j].classList.add("correctAnswer");
+          buttons[j].classList.add("correctAnswer")
           setTimeout(() => {
-            buttons[j].classList.remove("correctAnswer");
-          }, 1500);
+            buttons[j].classList.remove("correctAnswer")
+          }, 1500)
         }
       }
       if (button1.innerText === arreyOfQuestions[i].correct_answer) {
-        totalResult++;
+        totalResult++
       }
       if (button1.innerText !== arreyOfQuestions[i].correct_answer) {
-        button1.classList.add("selected");
+        button1.classList.add("selected")
       }
 
       setTimeout(function () {
-        button1.classList.remove("selected");
-      }, 1500);
+        button1.classList.remove("selected")
+      }, 1500)
       if (i === arreyOfQuestions.length - 1) {
-        clearInterval(interval);
-        window.location.href = "./Result.html";
+        clearInterval(interval)
+        window.location.href = "./Result.html"
       }
-      counterTimerColor = 0;
+      counterTimerColor = 0
       setTimeout(() => {
-        count = 1;
-      }, 1000);
-    };
+        count = 1
+      }, 1000)
+    }
     button2.onclick = function () {
       for (let j = 0; j < buttons.length; j++) {
         if (buttons[j].innerText === arreyOfQuestions[i].correct_answer) {
-          buttons[j].classList.add("correctAnswer");
+          buttons[j].classList.add("correctAnswer")
           setTimeout(() => {
-            buttons[j].classList.remove("correctAnswer");
-          }, 1500);
+            buttons[j].classList.remove("correctAnswer")
+          }, 1500)
         }
       }
 
       if (button2.innerText === arreyOfQuestions[i].correct_answer) {
-        totalResult++;
+        totalResult++
       }
       if (button2.innerText !== arreyOfQuestions[i].correct_answer) {
-        button2.classList.add("selected");
+        button2.classList.add("selected")
       }
       setTimeout(function () {
-        button2.classList.remove("selected");
-      }, 1500);
+        button2.classList.remove("selected")
+      }, 1500)
       if (i === arreyOfQuestions.length - 1) {
-        clearInterval(interval);
-        window.location.href = "./Result.html";
+        clearInterval(interval)
+        window.location.href = "./Result.html"
       }
-      counterTimerColor = 0;
+      counterTimerColor = 0
       setTimeout(() => {
-        count = 1;
-      }, 1000);
-    };
+        count = 1
+      }, 1000)
+    }
     button3.onclick = function () {
       for (let j = 0; j < buttons.length; j++) {
         if (buttons[j].innerText === arreyOfQuestions[i].correct_answer) {
-          buttons[j].classList.add("correctAnswer");
+          buttons[j].classList.add("correctAnswer")
           setTimeout(() => {
-            buttons[j].classList.remove("correctAnswer");
-          }, 1500);
+            buttons[j].classList.remove("correctAnswer")
+          }, 1500)
         }
       }
 
       if (button3.innerText === arreyOfQuestions[i].correct_answer) {
-        totalResult++;
+        totalResult++
       }
       if (button3.innerText !== arreyOfQuestions[i].correct_answer) {
-        button3.classList.add("selected");
+        button3.classList.add("selected")
       }
 
       setTimeout(function () {
-        button3.classList.remove("selected");
-      }, 1500);
+        button3.classList.remove("selected")
+      }, 1500)
       if (i === arreyOfQuestions.length - 1) {
-        clearInterval(interval);
-        window.location.href = "./Result.html";
+        clearInterval(interval)
+        window.location.href = "./Result.html"
       }
-      counterTimerColor = 0;
+      counterTimerColor = 0
       setTimeout(() => {
-        count = 1;
-      }, 1000);
-    };
+        count = 1
+      }, 1000)
+    }
     button4.onclick = function () {
       for (let j = 0; j < buttons.length; j++) {
         if (buttons[j].innerText === arreyOfQuestions[i].correct_answer) {
-          buttons[j].classList.add("correctAnswer");
+          buttons[j].classList.add("correctAnswer")
           setTimeout(() => {
-            buttons[j].classList.remove("correctAnswer");
-          }, 1500);
+            buttons[j].classList.remove("correctAnswer")
+          }, 1500)
         }
       }
 
       if (button4.innerText === arreyOfQuestions[i].correct_answer) {
-        totalResult++;
+        totalResult++
       }
       if (button4.innerText !== arreyOfQuestions[i].correct_answer) {
-        button4.classList.add("selected");
+        button4.classList.add("selected")
       }
       setTimeout(function () {
-        button4.classList.remove("selected");
-      }, 1500);
+        button4.classList.remove("selected")
+      }, 1500)
       if (i === arreyOfQuestions.length - 1) {
-        clearInterval(interval);
-        window.location.href = "./Result.html";
+        clearInterval(interval)
+        window.location.href = "./Result.html"
       }
-      counterTimerColor = 0;
+      counterTimerColor = 0
       setTimeout(() => {
-        count = 1;
-      }, 1000);
+        count = 1
+      }, 1000)
       //risultato incrementato ad ogni click della risposta esatta
-    };
+    }
 
     //cambio domande
     if (count === 0) {
-      i++;
-      count = 3;
-      h3.innerHTML = arrOfTitle[i];
+      i++
+      count = 3
+      h3.innerHTML = arrOfTitle[i]
       if (questionArr[i].length < 3) {
-        button3.style.display = "none";
-        button4.style.display = "none";
+        button3.style.display = "none"
+        button4.style.display = "none"
       } else if (questionArr[i].length > 2) {
-        button3.style.display = "block";
-        button4.style.display = "block";
+        button3.style.display = "block"
+        button4.style.display = "block"
       }
       for (let j = 0; j < questionArr[i].length; j++) {
-        buttons[j].innerText = questionArr[i][j];
+        buttons[j].innerText = questionArr[i][j]
       }
 
-      p.innerHTML = `question ${i + 1} <span>/10</span>`;
+      p.innerHTML = `question ${i + 1} <span>/10</span>`
     }
 
     // interruzione ciclo
     if (i === arreyOfQuestions.length - 1) {
-      const percentageResult = (totalResult / arreyOfQuestions.length) * 100;
+      const percentageResult = (totalResult / arreyOfQuestions.length) * 100
 
-      localStorage.setItem("correct", totalResult);
-      localStorage.setItem("correctPercentage", percentageResult);
-      localStorage.setItem("length", lengthArray);
+      localStorage.setItem("correct", totalResult)
+      localStorage.setItem("correctPercentage", percentageResult)
+      localStorage.setItem("length", lengthArray)
       // setInterval(function () {
       //   clearInterval(interval);
       //   window.location.href = "./Result.html";
       // }, 59500);
     }
     // }
-  }, 1000);
-  return count;
-};
-window.onload = timerCounter();
-
-//calcolare percentuale risposte
-
-/////////////////////////////////////////////////////////////////////////////
-const rateUs = document.getElementById("rateUsContainer");
-rateUs.addEventListener("click", () => {
-  window.location.href = "../feedback.html";
-});
-
-const correctAnswers = localStorage.getItem("correctPercentage");
-console.log(correctAnswers);
-const correct = document.querySelector("#correct h3");
-correct.innerText = correctAnswers + "%";
-const wrong = document.querySelector("#wrong h3");
-wrong.innerText = 100 - correctAnswers + "%";
-const graph = document.querySelector(".percentage");
-graphPercentage = 100 - correctAnswers;
-graph.style.background = `conic-gradient(rgb(210,0,148)0% ${graphPercentage}%,rgb(0,255,255)0%)`;
-
-const numOfCorrectQuestions = localStorage.getItem("correct");
-const numOfQuestions = localStorage.getItem("length");
-const numOfWrongQuestions = numOfQuestions - numOfCorrectQuestions;
-const correctQuestions = document.querySelector("#correct p");
-const wrongQuestions = document.querySelector("#wrong p");
-correctQuestions.innerText =
-  numOfCorrectQuestions + "/" + numOfQuestions + " questions";
-wrongQuestions.innerText =
-  numOfWrongQuestions + "/" + numOfQuestions + " questions";
-
-if (correctAnswers < 60) {
-  const noPassText = document.querySelector("#congratulations h1");
-  const noPassText2 = document.querySelector("#congratulations h3");
-  const noPassText3 = document.querySelector("#congratulations p");
-  noPassText.innerText = "Exam Failed";
-  noPassText2.innerText = "You haven't passed the exam";
-  noPassText3.innerText = "We'll contact you for further instruction";
+  }, 1000)
+  return count
 }
-
+window.onload = timerCounter()
